9708f4fda388660dd12ca3d1490e9439e1e31698
cygan3107 <krzysztof.malon@onet.pl>
2024-05-19 15:54:29 +0200

    commit

--- task-1.js
+++ task-1.js
@@ -1,5 +1,15 @@
 "use strict";
-function isEnoughCapacity(products, containerSize) {}
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+    for (const product of Object.values(products)) {
+        total += product;
+    }
+    if (total <= containerSize) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
